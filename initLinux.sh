65e74733b6bfdc44cc32ecf67ced135b6e62470f
Jamin <dev.jamin@gmail.com>
2022-12-09 14:47:24 -0500

    chore(initLinux): Add ssh-keygen and git config

--- initLinux.sh
+++ initLinux.sh
@@ -1,18 +1,74 @@
 #!/bin/bash
 
-curl -L https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.bashrc >> ~/.bashrc
+# .bashrc
+echo Creating/appending .bashrc
 
-rm -rf ~/.gitconfig && curl -Lo ~/.gitconfig https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.gitconfig
+sed -i '/# BEGIN OF https:\/\/raw.githubusercontent.com\/JaminMa\/ConfigsForNewTerminals\/master\/.bashrc/,/# END OF https:\/\/raw.githubusercontent.com\/JaminMa\/ConfigsForNewTerminals\/master\/.bashrc/d' ~/.bashrc
 
-rm -rf .vimrc && curl -Lo ~/.vimrc https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.vimrc
+curl -LSs https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.bashrc >> ~/.bashrc
 
-rm -rf .nvm && curl -L https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+# ------
+
+# NVM
+
+echo Installing Node Version Manager
+
+curl -LSs https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash > /dev/null
+
+# ------
+
+# VIM
+
+echo Configuring VIM and installing Pathogen, Syntastic, and Monokai color theme
+
+rm -rf ~/.vimrc && curl -LSs https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.vimrc > ~/.vimrc
 
 rm -rf ~/.vim
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -Lo ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+cd ~/.vim/bundle && git clone --quiet --depth=1 https://github.com/vim-syntastic/syntastic.git > /dev/null
 
-mkdir -p ~/.vim/colors && curl -Lo ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
+mkdir -p ~/.vim/colors && curl -LSso ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
+
+# ------
+
+# Git
+
+echo Configuring Git
+
+rm -rf ~/.gitconfig && curl -LSs https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.gitconfig > ~/.gitconfig
+
+echo Please enter your full name for Git config:
+
+read gitFullName
+
+git config --global user.name $gitFullName
+
+echo Please enter your email for Git config:
+
+read email
+
+git config --global user.email $email
+
+# ------
+
+# SSH Key
+
+while true; do
+  read -p "Generate SSH key? (Y/n)" yn
+  case $yn in
+    [Yy]*|"")
+      ssh-keygen
+      echo Remember to upload the new token to your GitHub account 
+      break;;
+    [Nn]*)
+      echo Skipping SSH key generation
+      break;;
+    *)
+      echo Please enter yes or no;;
+  esac
+done
+
+echo Script completed!
 
