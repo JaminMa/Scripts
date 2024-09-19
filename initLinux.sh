d04aff68e55dc41eb093ac25523cbacc9d71b6f9
Jamin Ma <JaminMa@users.noreply.github.com>
2024-09-18 22:25:35 -0400

    feat(vim): install vim if needed
    This change installs VIM if the script detects that VIM is not already installed.

--- initLinux.sh
+++ initLinux.sh
@@ -1,74 +1,75 @@
 #!/bin/bash
 
 # .bashrc
-echo Creating/appending .bashrc
-
+echo "Creating/appending .bashrc"
 sed -i '/# BEGIN OF https:\/\/raw.githubusercontent.com\/JaminMa\/ConfigsForNewTerminals\/master\/.bashrc/,/# END OF https:\/\/raw.githubusercontent.com\/JaminMa\/ConfigsForNewTerminals\/master\/.bashrc/d' ~/.bashrc
-
 curl -LSs https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.bashrc >> ~/.bashrc
 
 # ------
 
 # NVM
-
-echo Installing Node Version Manager
-
+echo "Installing Node Version Manager"
 curl -LSs https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash > /dev/null
 
 # ------
 
 # VIM
+echo "Checking for VIM installation"
+if ! command -v vim &> /dev/null; then
+    echo "VIM not found, installing..."
+    # Check package manager and install VIM
+    if command -v apt &> /dev/null; then
+        sudo apt update && sudo apt install -y vim || { echo "VIM installation failed"; exit 1; }
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y vim || { echo "VIM installation failed"; exit 1; }
+    elif command -v brew &> /dev/null; then
+        if ! command -v brew &> /dev/null; then
+            echo "Homebrew not found. Please install it first."
+            exit 1
+        fi
+        brew install vim || { echo "VIM installation failed"; exit 1; }
+    else
+        echo "Package manager not found. Please install VIM manually."
+        exit 1
+    fi
+else
+    echo "VIM is already installed."
+fi
 
-echo Configuring VIM and installing Pathogen, Syntastic, and Monokai color theme
-
+echo "Configuring VIM and installing Pathogen, Syntastic, and Monokai color theme"
 rm -rf ~/.vimrc && curl -LSs https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.vimrc > ~/.vimrc
-
 rm -rf ~/.vim
-
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
 cd ~/.vim/bundle && git clone --quiet --depth=1 https://github.com/vim-syntastic/syntastic.git > /dev/null
-
 mkdir -p ~/.vim/colors && curl -LSso ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 
 # ------
 
 # Git
-
-echo Configuring Git
-
+echo "Configuring Git"
 rm -rf ~/.gitconfig && curl -LSs https://raw.githubusercontent.com/JaminMa/ConfigsForNewTerminals/master/.gitconfig > ~/.gitconfig
-
-echo Please enter your full name for Git config:
-
-read gitFullName
-
-git config --global user.name $gitFullName
-
-echo Please enter your email for Git config:
-
-read email
-
-git config --global user.email $email
+read -p "Please enter your full name for Git config: " gitFullName
+git config --global user.name "$gitFullName"
+read -p "Please enter your email for Git config: " email
+git config --global user.email "$email"
 
 # ------
 
 # SSH Key
-
 while true; do
   read -p "Generate SSH key? (Y/n) " yn
   case $yn in
     [Yy]*|"")
       ssh-keygen
-      echo Remember to upload the new token to your GitHub account 
+      echo "Remember to upload the new token to your GitHub account"
       break;;
     [Nn]*)
-      echo Skipping SSH key generation
+      echo "Skipping SSH key generation"
       break;;
     *)
-      echo Please enter yes or no;;
+      echo "Please enter yes or no";;
   esac
 done
 
-echo Script completed!
-
+# End Script
+echo "Script completed!"
